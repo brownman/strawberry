891e0b307c93c16b031b73dec33cebc5f8acdc94
Dmitry A. Ustalov <eveeel@gmail.com>
2009-11-30 04:21:03 +0500

    strawberry.rb rdoc

--- lib/strawberry.rb
+++ lib/strawberry.rb
@@ -1,7 +1,12 @@
 # encoding: utf-8
 
-require 'strawberry/tokyo'
-require 'strawberry/uuid'
-require 'strawberry/base'
-require 'strawberry/dao'
-require 'strawberry/node'
+# The Strawberry library provides a lightweight and
+# easy solution to store hierarchical 2D-tables with
+# metadata.
+module Strawberry
+  require 'strawberry/tokyo'
+  require 'strawberry/uuid'
+  require 'strawberry/base'
+  require 'strawberry/dao'
+  require 'strawberry/node'
+end
