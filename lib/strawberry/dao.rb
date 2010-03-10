07cbe784e9887973b2a94ed79f19b76a25c4419f
Dmitry A. Ustalov <eveeel@gmail.com>
2010-03-10 18:48:03 +0500

    dao rewritten using oklahoma_mixer

--- lib/strawberry/dao.rb
+++ lib/strawberry/dao.rb
@@ -2,6 +2,8 @@
 
 module Strawberry
   class DAO
+    require 'oklahoma_mixer'
+
     VALID_NAME_PATTERN = /^[A-z_\-0-9]+$/
 
     def valid_name?(name)
@@ -49,28 +51,22 @@ module Strawberry
       end
     end
 
-    #attr_reader :index, :data, :meta, :path
-    #private :index, :data, :meta
-    attr_reader :index, :data, :meta, :path
-    private :index, :data, :meta
+    attr_reader :index_path, :data_path, :meta_path
+    private :index_path, :data_path, :meta_path
 
     def initialize(path)
-      @path = path
-
-      index_path = File.join @path, 'index.tct'
-      @index = Tokyo::Table.new(index_path)
-
-      data_path = File.join @path, 'database.tcb'
-      @data = Tokyo::Cabinet.new(data_path)
-
-      meta_path = File.join @path, 'metabase.tct'
-      @meta = Tokyo::Table.new(meta_path)
+      @index_path = File.join path, 'index.tct'
+      @data_path  = File.join path, 'database.tcb'
+      @meta_path  = File.join path, 'metabase.tct'
     end
 
     # Check the table existance with <tt>id</td>.
     def have_table? id
       return true unless id
-      valid_name?(id) && index[id]
+      raise InvalidName.new(id) unless valid_name?(id)
+      session(index_path) do |index|
+        index.include? id
+      end
     end
 
     # Check the table existance with <tt>name</td>,
@@ -78,17 +74,25 @@ module Strawberry
     def have_named_table? name, parent_id = nil
       return true unless name
       return false unless valid_name?(name)
+      raise NotFound.new(parent_id) unless have_table?(parent_id)
 
-      !!get_childs(parent_id).find do |c|
-        index[c]['name'] == name
-      end
+      session(index_path) do |index|
+        index.all :conditions => [
+          [ :parent, :==, parent_id ],
+          [ :name, :==, name ]
+        ]
+      end.size != 0
     end
 
     # Returns the table name by its <tt>id</tt>.
     def get_name(id)
       raise InvalidName.new(id) unless valid_name?(id)
-      raise NotFound.new(id) unless have_table? id
-      index[id]['name']
+      raise NotFound.new(id) unless have_table?(id)
+
+      session(index_path) do |index|
+        row = index.fetch(id, {})
+        row['name']
+      end
     end
 
     # Adds a new table with specified <tt>name</tt> and
@@ -101,94 +105,107 @@ module Strawberry
       end
 
       id = Strawberry.uuid
-      index[id] = { 'parent' => parent_id, 'name' => name }
+      session(index_path) do |index|
+        index.store(id, { 'parent' => parent_id, 'name' => name })
+      end
       id
     end
 
     # Returns the data of table <tt>id</tt>.
     def get_data(id)
       raise InvalidName.new(id) unless valid_name?(id)
-      raise NotFound.new(id) unless have_table? id
+      raise NotFound.new(id) unless have_table?(id)
 
-      read = data[id].map { |uuid| data[uuid] }
+      result = session(data_path) do |db|
+        db.values(id).map do |uuid|
+          db.values(uuid)
+        end
+      end
 
-      # enjoy
-      array_wrap(read).freeze
+      array_wrap(result).freeze
     end
 
     # Sets and returns the <tt>new_data</tt> of table <tt>id</tt>.
     def set_data(id, new_data)
       raise InvalidName.new(id) unless valid_name?(id)
-      raise NotFound.new(id) unless have_table? id
+      raise NotFound.new(id) unless have_table?(id)
 
-      saved = array_wrap(new_data)
+      result = array_wrap(new_data)
 
-      remove_data id
-
-      # TODO: transactions will be nice
-      data[id] = saved.map do |array|
-        uuid = Strawberry.uuid
-        data[uuid] = array
-        uuid
+      rewrite_data id do |db|
+        db.transaction do
+          idx = result.map do |arr|
+            uuid = Strawberry.uuid
+            db.store(uuid, arr, :dup)
+            uuid
+          end
+          db.store(id, idx, :dup)
+        end
       end
 
-      saved.freeze
+      result.freeze
     end
 
     # Returns the metadata of table <tt>id</tt>.
     def get_meta(id)
       raise InvalidName.new(id) unless valid_name?(id)
-      raise NotFound.new(id) unless have_table? id
+      raise NotFound.new(id) unless have_table?(id)
 
-      (meta[id] || {}).freeze
+      session(meta_path) do |meta|
+        meta.fetch(id, {}).freeze
+      end
     end
 
     # Sets and returns the <tt>new_meta</tt> of table <tt>id</tt>.
     def set_meta(id, new_meta)
       raise InvalidName.new(id) unless valid_name?(id)
-      raise NotFound.new(id) unless have_table? id
+      raise NotFound.new(id) unless have_table?(id)
       raise TypeError unless new_meta.instance_of? Hash
 
-      (meta[id] = new_meta).freeze
+      session(meta_path) do |meta|
+        meta.store(id, new_meta).freeze
+      end
     end
 
     # Returns the parent id of table <tt>id</tt>.
     def get_parent id
       raise InvalidName.new(id) unless valid_name?(id)
-      raise NotFound.new(id) unless have_table? id
+      raise NotFound.new(id) unless have_table?(id)
 
-      parent_id = index[id]['parent']
+      parent_id = session(index_path) do |index|
+        row = index.fetch(id, {})
+        row['parent']
+      end
       parent_id && parent_id.empty? ? nil : parent_id
     end
 
     # Returns the array of child's ids of table <tt>id</tt>.
     def get_childs id
-      raise NotFound.new(id) unless have_table? id
+      raise NotFound.new(id) unless have_table?(id)
 
-      index.query do |q|
-        q.add_condition :parent, :eq, id
-      end.map { |r| r[:pk] }
+      session(index_path) do |index|
+        index.all :conditions => [ :parent, :==, id ]
+      end.map { |r| r.first }.freeze
     end
-
-    def remove_data id
-      data[id].each { |uuid| data.delete uuid }
-      data.delete id
-      nil
-    end
-    private :remove_data
 
     # Drop table <tt>id</tt> and return it's <tt>id</tt>.
     def remove_table id
       raise InvalidName.new(id) unless valid_name?(id)
-      raise NotFound.new(id) unless have_table? id
+      raise NotFound.new(id) unless have_table?(id)
 
       get_childs(id).each do |c|
         remove_table c if have_table? c
       end
 
-      remove_data id
-      meta.delete id
-      index.delete id
+      session(index_path) do |index|
+        index.transaction do
+          session(meta_path) do |meta|
+            meta.delete id
+          end
+          rewrite_data id
+          index.delete id
+        end
+      end
 
       id
     end
@@ -199,5 +216,33 @@ module Strawberry
       obj.map { |l| Array(l).map { |c| c.to_s } }
     end
     private :array_wrap
+
+    def session(path)
+      raise LocalJumpError unless block_given?
+      db = OKMixer.open(path)
+      begin
+        yield db
+      rescue => ex
+        raise ex if defined? STRAWBERRY_DEBUG
+        nil
+      ensure
+        db.close
+      end
+    end
+    protected :session
+
+    def rewrite_data id
+      session(data_path) do |db|
+        db.transaction do
+          db.values(id).each do |uuid|
+            db.delete(uuid, :dup)
+          end
+          db.delete(id, :dup)
+        end
+        yield db if block_given?
+      end
+      nil
+    end
+    protected :rewrite_data
   end
 end
